a0b343d73845c92352503e265653293e0d0b946d
Nobleson <noblesity@gmail.com.com>
2019-06-01 14:07:22 +0100

    update

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -2,25 +2,25 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import { interopDefault } from './utils'
 
-const _69ffe801 = () => interopDefault(import('..\\pages\\cslmis\\application\\index.vue' /* webpackChunkName: "pages_cslmis_application_index" */))
-const _05712b6c = () => interopDefault(import('..\\pages\\cslmis\\artisans\\index.vue' /* webpackChunkName: "pages_cslmis_artisans_index" */))
-const _b5322d5a = () => interopDefault(import('..\\pages\\cslmis\\centers\\index.vue' /* webpackChunkName: "pages_cslmis_centers_index" */))
-const _47464456 = () => interopDefault(import('..\\pages\\cslmis\\companies\\index.vue' /* webpackChunkName: "pages_cslmis_companies_index" */))
-const _2d917ae5 = () => interopDefault(import('..\\pages\\cslmis\\dashboard.vue' /* webpackChunkName: "pages_cslmis_dashboard" */))
-const _10640540 = () => interopDefault(import('..\\pages\\cslmis\\facility\\index.vue' /* webpackChunkName: "pages_cslmis_facility_index" */))
-const _fe25b2c0 = () => interopDefault(import('..\\pages\\cslmis\\jobadvert\\index.vue' /* webpackChunkName: "pages_cslmis_jobadvert_index" */))
-const _04f289d0 = () => interopDefault(import('..\\pages\\cslmis\\jobapplication\\index.vue' /* webpackChunkName: "pages_cslmis_jobapplication_index" */))
-const _027269a0 = () => interopDefault(import('..\\pages\\cslmis\\labourstatistic\\index.vue' /* webpackChunkName: "pages_cslmis_labourstatistic_index" */))
-const _dd6e04f0 = () => interopDefault(import('..\\pages\\cslmis\\login\\index.vue' /* webpackChunkName: "pages_cslmis_login_index" */))
-const _2a8f8540 = () => interopDefault(import('..\\pages\\cslmis\\register\\index.vue' /* webpackChunkName: "pages_cslmis_register_index" */))
-const _32ab1ead = () => interopDefault(import('..\\pages\\cslmis\\retrenchement\\index.vue' /* webpackChunkName: "pages_cslmis_retrenchement_index" */))
-const _294326a4 = () => interopDefault(import('..\\pages\\dashboard\\dashboard.vue' /* webpackChunkName: "pages_dashboard_dashboard" */))
-const _362ff0d6 = () => interopDefault(import('..\\pages\\pages\\emailVerify.vue' /* webpackChunkName: "pages_pages_emailVerify" */))
-const _47f0f689 = () => interopDefault(import('..\\pages\\pages\\Page404.vue' /* webpackChunkName: "pages_pages_Page404" */))
-const _7c9ed3c6 = () => interopDefault(import('..\\pages\\pages\\Page500.vue' /* webpackChunkName: "pages_pages_Page500" */))
-const _32f10a2e = () => interopDefault(import('..\\pages\\cslmis\\centers\\training-center.vue' /* webpackChunkName: "pages_cslmis_centers_training-center" */))
-const _448c173d = () => interopDefault(import('..\\pages\\cslmis\\trades\\Trades.vue' /* webpackChunkName: "pages_cslmis_trades_Trades" */))
-const _bfea5044 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
+const _8205e8d2 = () => interopDefault(import('..\\pages\\cslmis\\application\\index.vue' /* webpackChunkName: "pages_cslmis_application_index" */))
+const _6378de74 = () => interopDefault(import('..\\pages\\cslmis\\artisans\\index.vue' /* webpackChunkName: "pages_cslmis_artisans_index" */))
+const _3afd702e = () => interopDefault(import('..\\pages\\cslmis\\centers\\index.vue' /* webpackChunkName: "pages_cslmis_centers_index" */))
+const _b1795528 = () => interopDefault(import('..\\pages\\cslmis\\companies\\index.vue' /* webpackChunkName: "pages_cslmis_companies_index" */))
+const _bb6052e2 = () => interopDefault(import('..\\pages\\cslmis\\dashboard.vue' /* webpackChunkName: "pages_cslmis_dashboard" */))
+const _5dff718a = () => interopDefault(import('..\\pages\\cslmis\\facility\\index.vue' /* webpackChunkName: "pages_cslmis_facility_index" */))
+const _3e2b9094 = () => interopDefault(import('..\\pages\\cslmis\\jobadvert\\index.vue' /* webpackChunkName: "pages_cslmis_jobadvert_index" */))
+const _4302b67a = () => interopDefault(import('..\\pages\\cslmis\\jobapplication\\index.vue' /* webpackChunkName: "pages_cslmis_jobapplication_index" */))
+const _f3305b94 = () => interopDefault(import('..\\pages\\cslmis\\labourstatistic\\index.vue' /* webpackChunkName: "pages_cslmis_labourstatistic_index" */))
+const _2576099e = () => interopDefault(import('..\\pages\\cslmis\\login\\index.vue' /* webpackChunkName: "pages_cslmis_login_index" */))
+const _50e9b18a = () => interopDefault(import('..\\pages\\cslmis\\register\\index.vue' /* webpackChunkName: "pages_cslmis_register_index" */))
+const _3cedb4c3 = () => interopDefault(import('..\\pages\\cslmis\\retrenchement\\index.vue' /* webpackChunkName: "pages_cslmis_retrenchement_index" */))
+const _851f9a8c = () => interopDefault(import('..\\pages\\dashboard\\dashboard.vue' /* webpackChunkName: "pages_dashboard_dashboard" */))
+const _07f5212b = () => interopDefault(import('..\\pages\\pages\\emailVerify.vue' /* webpackChunkName: "pages_pages_emailVerify" */))
+const _d4f369c2 = () => interopDefault(import('..\\pages\\pages\\Page404.vue' /* webpackChunkName: "pages_pages_Page404" */))
+const _6b97af48 = () => interopDefault(import('..\\pages\\pages\\Page500.vue' /* webpackChunkName: "pages_pages_Page500" */))
+const _42c515ff = () => interopDefault(import('..\\pages\\cslmis\\centers\\training-center.vue' /* webpackChunkName: "pages_cslmis_centers_training-center" */))
+const _fcb3145a = () => interopDefault(import('..\\pages\\cslmis\\trades\\Trades.vue' /* webpackChunkName: "pages_cslmis_trades_Trades" */))
+const _97f43af0 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
 
 Vue.use(Router)
 
@@ -98,79 +98,79 @@ export function createRouter() {
 
     routes: [{
       path: "/cslmis/application",
-      component: _69ffe801,
+      component: _8205e8d2,
       name: "cslmis-application"
     }, {
       path: "/cslmis/artisans",
-      component: _05712b6c,
+      component: _6378de74,
       name: "cslmis-artisans"
     }, {
       path: "/cslmis/centers",
-      component: _b5322d5a,
+      component: _3afd702e,
       name: "cslmis-centers"
     }, {
       path: "/cslmis/companies",
-      component: _47464456,
+      component: _b1795528,
       name: "cslmis-companies"
     }, {
       path: "/cslmis/dashboard",
-      component: _2d917ae5,
+      component: _bb6052e2,
       name: "cslmis-dashboard"
     }, {
       path: "/cslmis/facility",
-      component: _10640540,
+      component: _5dff718a,
       name: "cslmis-facility"
     }, {
       path: "/cslmis/jobadvert",
-      component: _fe25b2c0,
+      component: _3e2b9094,
       name: "cslmis-jobadvert"
     }, {
       path: "/cslmis/jobapplication",
-      component: _04f289d0,
+      component: _4302b67a,
       name: "cslmis-jobapplication"
     }, {
       path: "/cslmis/labourstatistic",
-      component: _027269a0,
+      component: _f3305b94,
       name: "cslmis-labourstatistic"
     }, {
       path: "/cslmis/login",
-      component: _dd6e04f0,
+      component: _2576099e,
       name: "cslmis-login"
     }, {
       path: "/cslmis/register",
-      component: _2a8f8540,
+      component: _50e9b18a,
       name: "cslmis-register"
     }, {
       path: "/cslmis/retrenchement",
-      component: _32ab1ead,
+      component: _3cedb4c3,
       name: "cslmis-retrenchement"
     }, {
       path: "/dashboard/dashboard",
-      component: _294326a4,
+      component: _851f9a8c,
       name: "dashboard-dashboard"
     }, {
       path: "/pages/emailVerify",
-      component: _362ff0d6,
+      component: _07f5212b,
       name: "pages-emailVerify"
     }, {
       path: "/pages/Page404",
-      component: _47f0f689,
+      component: _d4f369c2,
       name: "pages-Page404"
     }, {
       path: "/pages/Page500",
-      component: _7c9ed3c6,
+      component: _6b97af48,
       name: "pages-Page500"
     }, {
       path: "/cslmis/centers/training-center",
-      component: _32f10a2e,
+      component: _42c515ff,
       name: "cslmis-centers-training-center"
     }, {
       path: "/cslmis/trades/Trades",
-      component: _448c173d,
+      component: _fcb3145a,
       name: "cslmis-trades-Trades"
     }, {
       path: "/",
-      component: _bfea5044,
+      component: _97f43af0,
       name: "index"
     }],
 
