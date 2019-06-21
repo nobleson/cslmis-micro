4db1af9838a0c7c6d854d45f6fbdbbfe648897e6
Nobleson <noblesity@gmail.com.com>
2019-06-22 00:06:27 +0100

    update

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,14 @@
     <b-navbar toggleable="lg" class="top">
         <div class="container">
         <b-navbar-brand href="#"><img src="~/assets/images/logo.png"></b-navbar-brand>
-
-        <b-navbar-toggle target="nnpm av-collapse">
           
-        </b-navbar-toggle>
-
+        <b-navbar-toggle target="nnpm av-collapse">
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item href="#">
+                    <b-button @click="login">login</b-button>
+                </b-nav-item>
+            </b-navbar-nav>
+          </b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
             <!-- Right aligned nav items -->
             <b-navbar-nav class="ml-auto">
@@ -18,7 +21,7 @@
             </b-navbar-nav>
         </b-collapse>
         
-        </div>
+   </div>
     </b-navbar>
     
       
